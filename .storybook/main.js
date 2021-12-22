8fee50e8c3ffcd3047067687ff04af5c2a5a4584
y4shiro <y4shironao@gmail.com>
2021-12-22 23:16:54 +0900

    Storybook の設定 main.js / preview.js を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../src/**/*.stories.jsx'], // ここを書き換える（`js`ではなく`jsx`であることに注意）
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: (config) => {
+    config.resolve.alias['~'] = path.join(__dirname, '../src/');
+    return config;
+  },
 };
