b630c09482dd22f13fbe0734154301a0148d05ae
y4shiro <y4shironao@gmail.com>
2021-12-23 11:44:39 +0900

    Global Style を Storybook に適用

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { addDecorator } from '@storybook/react';
+import GlobalStyle from '../src/style/GlobalStyle';
 
 addDecorator((storyFn) => (
   <MemoryRouter initialEntries={['/', 'posts']}>{storyFn()}</MemoryRouter>
+));
+
+addDecorator((storyFn) => (
+  <>
+    <GlobalStyle />
+    {storyFn()}
+  </>
 ));
