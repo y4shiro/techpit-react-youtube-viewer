122ee80d1ab79626013ee01688924d45e8ef8842
y4shiro <y4shironao@gmail.com>
2021-12-24 10:26:43 +0900

    organisms/VideosList コンポーネントとその story を作成

---
+++ src/components/organisms/VideosList/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import VideosListItem from '~/components/organisms/VideosListItem';
+import Spinner from '~/components/atoms/Spinner';
+import Typography from '~/components/atoms/Typography';
+
+const StyledVideosListItem = styled(VideosListItem)`
+  margin-top: 10px;
+`;
+
+const Loading = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100px;
+`;
+
+const VideosList = ({ loading, videos }) => (
+  <>
+    {!loading && !videos.length && <Typography>ビデオがありません</Typography>}
+    {videos.map((video) => (
+      <StyledVideosListItem key={video.id} video={video} />
+    ))}
+    {loading && (
+      <Loading>
+        <Spinner />
+      </Loading>
+    )}
+  </>
+);
+
+VideosList.propTypes = {
+  videos: PropTypes.arrayOf(PropTypes.shape({})),
+  loading: PropTypes.bool,
+};
+
+VideosList.defaultProps = {
+  videos: [],
+  loading: false,
+};
+
+export default VideosList;
