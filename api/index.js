8be5271bb170fa6a5cc3d6777ffc49be621c6c69
y4shiro <y4shironao@gmail.com>
2021-12-29 17:35:52 +0900

    動画詳細情報取得 / 関連動画取得  API の作成

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { google } = require('googleapis');
 
+// 取得したAPIキーを設定する
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 const youtube = google.youtube({
@@ -20,6 +21,48 @@ router.get('/videos/search/:keyword', (req, res, next) => {
     } = await youtube.search.list({
       part: 'id',
       q: keyword,
+      type: 'video',
+      maxResults: 20,
+      pageToken,
+    });
+    // 動画の情報を取得
+    const ids = idItems.map(({ id: { videoId } }) => videoId);
+    const {
+      data: { items },
+    } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: ids.join(','),
+    });
+    res.json({ items, nextPageToken });
+  })().catch(next);
+});
+
+// 動画詳細情報取得
+router.get('/videos/:videoId', (req, res, next) => {
+  const { videoId } = req.params;
+  (async () => {
+    // 動画の情報を取得
+    const {
+      data: { items },
+    } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: videoId,
+    });
+    res.json(items[0]);
+  })().catch(next);
+});
+
+// 関連動画取得
+router.get('/videos/:videoId/related', (req, res, next) => {
+  const { videoId: relatedToVideoId } = req.params;
+  const { pageToken } = req.query;
+  (async () => {
+    // 関連動画のIDを取得
+    const {
+      data: { items: idItems, nextPageToken },
+    } = await youtube.search.list({
+      part: 'id',
+      relatedToVideoId,
       type: 'video',
       maxResults: 20,
       pageToken,
