851286eb3b384070377725ccf6b3a46cdaf8e5af
y4shiro <y4shironao@gmail.com>
2022-01-06 16:23:55 +0900

    トップページのビデオリストにお気に入りボタンを表示

--- src/components/pages/TopPage/index.jsx
+++ src/components/pages/TopPage/index.jsx
@@ -21,7 +21,9 @@ export const TopPagePresenter = ({
     searchFormContents={
       <SearchForm onSubmit={search} defaultValue={defaultKeyword} />
     }
-    videosListContents={<VideosList videos={videos} loading={loading} />}
+    videosListContents={
+      <VideosList videos={videos} loading={loading} withFavoriteButton />
+    }
     onScrollEnd={searchNext}
   />
 );
